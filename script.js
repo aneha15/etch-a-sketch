dd2f634c57eca8a4f54649c7515a1f179990f7fa
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2024-12-02 22:15:24 +0600

    Add hover effect

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const grid = document.querySelector(".grid");
 
 for (let i = 0; i < 16; i++) {
    for (let j = 0; j < 16; j++) {
-       const smallDiv = document.createElement("div");
+    const smallDiv = document.createElement("div");
        smallDiv.style.cssText = "border: 0.5px solid gray; height: 40px; width: 40px; box-sizing: border-box;";
        grid.appendChild(smallDiv);
+       smallDiv.addEventListener("mouseenter", () => {
+        smallDiv.style.backgroundColor = "lightpink";
+       });
     }
 }
+
+
+
+
 
