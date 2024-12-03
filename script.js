64fb8d7853f68b6e659a92fc8430afe394354a23
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2024-12-03 11:07:59 +0600

    Reset grid size

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 const grid = document.querySelector(".grid");
 const form = document.querySelector("form");
+const replay = document.createElement("button");
+replay.textContent = "Replay";
+replay.style.marginTop = "16px";
 
 function printGrid (size) {
 
@@ -11,25 +14,34 @@ function printGrid (size) {
             smallDiv.style.height = smallDiv.style.width = `${640/size}px`;
             grid.appendChild(smallDiv);
             smallDiv.addEventListener("mouseenter", () => {
-             smallDiv.style.backgroundColor = "lightpink";
-            });
+             smallDiv.style.backgroundColor = "turquoise";
+            })
         }
      
      }
 }
+function reset () {
+    replay.addEventListener("click", (event) => {
+        event.preventDefault();
+        grid.innerHTML = "";
+        printGrid(16);
+    });
+    form.appendChild(replay);
+}
 
+function colorPicker (elem) {
+    elem.style.backgroundColor = `rgb`
+ }
+
+
+printGrid(16);
 
 const btn = document.querySelector("button").addEventListener("click", (event) => {
     event.preventDefault();
     let size = parseInt(document.querySelector("#size").value);
+    grid.innerHTML = "";
     printGrid(size);
-    const replay = document.createElement("button");
-    replay.textContent = "Replay";
-    replay.style.marginTop = "16px";
-    form.appendChild(replay);
-    replay.addEventListener("click", () => {
-        location.reload();
-    })
+    reset();
 });
 
 
